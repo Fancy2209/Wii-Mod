f654a97a41521d96dc4fe9da522c65efca9783e0
Fancy2209 <paulo.medeiros1302@gmail.com>
2022-03-05 22:37:46 -0100

    Actual Mod

---
+++ WiiButtonPromptsModule.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Celeste.Mod.WiiButtonPrompts {
+    public class WiiButtonPromptsModule : EverestModule {
+        
+        public static WiiButtonPromptsModule Instance;
+
+        public WiiButtonPromptsModule() {
+            Instance = this;
+        }
+
+        public override void Load() {
+        }
+
+        public override void Unload() {
+        }
+
+    }
+}
