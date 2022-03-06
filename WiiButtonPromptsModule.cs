7b3cc5f5dc0a11b3bc58316dd30ac54ea3e50cca
Fancy2209 <paulo.medeiros1302@gmail.com>
2022-03-06 20:23:52 -0100

    dfs

--- WiiButtonPromptsModule.cs
+++ WiiButtonPromptsModule.cs
@@ -1,26 +1,37 @@
 ﻿using System;
-using Microsoft.Xna.Framework; 
+using Celeste;
+using Microsoft.Xna.Framework;
+using Monocle;
 
 namespace Celeste.Mod.WiiButtonPrompts {
     public class WiiButtonPromptsModule : EverestModule {
         
         public static WiiButtonPromptsModule Instance;
+        
 
-        public WiiButtonPromptsModule() {
+        public WiiButtonPromptsModule() {   
             Instance = this;
             
         }
-
         public override void Load() {
-            //On.Celeste.Input.GuiInputPrefix += WiiButtons;
-            string guid = GamePad.GetGUIDEXT(index);        
-            Console.WriteLine(guid);
+            On.Celeste.Input.GuiInputPrefix += WiiButtons;
+            
         }
         public override void Unload() {
-            //On.Celeste.Input.GuiInputPrefix -=  WiiButtons;
+            On.Celeste.Input.GuiInputPrefix -=  WiiButtons;
+        }
+        
+        public override void Initialize() {
+           string gUIDEXT = Microsoft.Xna.Framework.Input.GamePad.GetGUIDEXT(MInput.GamePads[Input.Gamepad].PlayerIndex);
+           Console.WriteLine(gUIDEXT);
+
         }
 
-        //private string WiiButtons(On.Celeste.Input.orig_GuiInputPrefix orig, Input.PrefixMode mode);
-          
+        private string WiiButtons(On.Celeste.Input.orig_GuiInputPrefix orig, Input.PrefixMode mode);
+            if (gUIDEXT.Equals("7e050603"))
+                {
+                    return "wii";
+                }
+                On.Celeste.Input.orig_GuiInputPrefix orig, On.Celeste.Input.PrefixMode mode; 
     }
 }
